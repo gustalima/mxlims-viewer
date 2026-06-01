2b167a5c64314ff7af9c10a88c4c1fe0793dff02
Gustavo Lima <gustavo.lima@astx.com>
2026-06-01 09:13:53 +0100

    feat: add Firebase configuration file for project setup

---
+++ .firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAjr8_hvo_uFQJSOBzKVPsXjvbhjSFJ3vs',
+    authDomain: 'mxlims-viewer.firebaseapp.com',
+    projectId: 'mxlims-viewer',
+    storageBucket: 'mxlims-viewer.appspot.com',
+    messagingSenderId: '842667311396',
+    appId: '1:842667311396:web:cfb60de167f33dd4fc33b1',
+    measurementId: 'G-DYY54SLT9K',
+}
